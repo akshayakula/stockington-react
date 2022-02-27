f07f8f8a2085a422746eb6dcaeb1c007dbf677c1
Keerthana Adapa <keerthanaadapa@vt.edu>
2022-02-27 02:57:50 -0500

    validation

--- src/App.js
+++ src/App.js
@@ -10,18 +10,48 @@ import bull from './Stockingtonlogo.png'
 
 function App() {
   
-  let readFile = ("nyse.csv", "utf8", (error, textContent) => {
-    if(error){ throw error; }
-    const parsedData = [];
-    for(let row of textContent.split("\n")){
-      const rowItems = row.split(",");
-      set_list.add(rowItems[0].toString());
+  let readFile = () => {
+    var file = new File([""], "/Users/keerthana/Desktop/VTHacks2022/stockington-react/src/nyse.txt");
+    console.log(file.size);
+    // var fr =new FileReader();
+    // fr.onload=function(){
+    //     console.log(fr.result);
+    // }
+    // var file = new File()
+    // fr.readAsText();
+    // });
+  }
+  
+  
+  //   const lineReader = require('line-reader');
+  //   const f = require('fs');
+  //   lineReader.eachLine('./nyse.txt',(line,last)=>{
+  //   console.log(line);
+  //   })
+  //   // if(error){ throw error; }
+  //   // const parsedData = [];
+  //   // for(let row of textContent.split("\n")){
+  //   //   const rowItems = row.split(",");
+      
+  //   //   set_list.add(rowItems[0].toString());
+  //   // }
+
+  // };
+  // 
+  readFile();
+
+  let validateTicker = (query) => {
+    console.log(query);
+    if (set_list.has(query))
+    {
+      setValidation(true);
+      setSpinner(true);
     }
-
-  });
-
-  let checkTicker = (query) => {
-    return set_list.has(query);
+    else 
+    {
+      setValidation(false);
+      setSpinner(false);
+    }
   }
 
   let [ticker, setTicker] = useState("");
@@ -30,16 +60,6 @@ function App() {
   let [validation, setValidation] = useState(true);
   let [query, setQuery] = useState("")
 
-  let set_list = new Set();
-
-  let validateTicker = (val) => {
-    if( val == "AAPL"){
-      setValidation(True)
-    }
-    else{
-      setValidation(False)
-    }
-  }
  
   return (
     <div className="App">
@@ -47,19 +67,19 @@ function App() {
         <br></br>
         <br></br>
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <img src={bull} class="img-fluid logo" alt="image"></img>
+        <img src={bull} className="img-fluid logo" alt="image"></img>
         <br></br>
         <h1><text className='text-success'></text>ASK <text className='text-warning'>STOCKINGTON</text></h1>
         <h4 className="text-light">Enter a stock ticker to get a <text className='text-success'>summary</text> of the most recent annual <text className='text-success'>finance</text> reports</h4>
         <br/>
         {/* <Stack direction="horizontal" gap={3}> */}
           <Form.Group className="mb-3" className = "input float-left"  controlId="formStockTicker" >
-            <Form.Control type="email" placeholder="AAPL, MCD, TSLA, ..." onChange={event => setTicker(event.target.value)} />
+            <Form.Control type="email" placeholder="AAPL, MCD, TSLA, ..." onChange={event => setTicker(event.target.value.toUpperCase())} />
             {!validation ? <Form.Text>Unacceptable ticker</Form.Text> : null }
             
           </Form.Group>
           <br></br>
-          <Button className="mb-3" size="lg" variant="outline-success" class="mybutton float-right" type="submit" onClick={() => {validateTicker(ticker)} }>
+          <Button className="mb-3" size="lg" variant="outline-success" className="mybutton float-right" type="submit" onClick={() => {validateTicker(ticker)} }>
             Submit
           </Button>
         {/* </Stack> */}
@@ -67,9 +87,9 @@ function App() {
         <br></br>
     
         <Stack direction="horizontal" className="justify-content-center" gap={3}>
-          {spinner ? <Spinner className="mb-3 loader" class="ratio ratio-1x1" animation="grow" variant="light" /> : null }
-          {spinner ? <Spinner className="mb-3 loader" class="ratio ratio-1x1" animation="grow" variant="light" /> : null }
-          {spinner ? <Spinner className="mb-3 loader" class="ratio ratio-1x1" animation="grow" variant="light" /> : null }
+          {spinner ? <Spinner className="mb-3 loader" className="ratio ratio-1x1" animation="grow" variant="light" /> : null }
+          {spinner ? <Spinner className="mb-3 loader" className="ratio ratio-1x1" animation="grow" variant="light" /> : null }
+          {spinner ? <Spinner className="mb-3 loader" className="ratio ratio-1x1" animation="grow" variant="light" /> : null }
         </Stack>
         
       </header>
