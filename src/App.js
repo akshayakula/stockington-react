5a6b0b4976c54b75511e7cd899685de0f00e5713
akshayakula <akshayakula@vt.edu>
2022-02-26 19:28:41 -0500

    edits

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import TextField from '@mui/material/TextField';
 
 function App() {
+  
+  let num = 5;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TextField
+          id="outlined-password-input"
+          label="Enter a ticker symbol"
+          color="primary"
+          focused
+        />
+        <text>{num}</text>
       </header>
     </div>
   );
